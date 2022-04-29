a91c2703ead4066bc6e5c65e3040b5700cad67a5
nullbr <bmarianoleite3@gmail.com>
2022-04-29 16:55:36 -0300

    finished api implementation

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,25 +1,56 @@
 require 'csv'
+require 'google/apis/civicinfo_v2'
+require 'erb'
+
+def clean_zipcode(zipcode)
+  # if zip code was not filled, it becomes the wrong zip code default of 00000
+  # if the zip code is lass than five digits, add zeros the the front until it becomes five digits
+  # if the zip code is more than five digits, truncate it to the five digits
+  # if the zip code is exactly five digits, assume that it is ok
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def legislator_by_zipcode(zipcode)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  civic_info.representative_info_by_address(
+    address: zipcode,
+    levels: 'country',
+    roles: %w[legislatorUpperBody legislatorLowerBody]
+  ).officials
+rescue StandardError
+  'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+end
+
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
 puts 'Event Manager Initialized!'
 
 contents = CSV.open('event_attendees.csv',
                     headers: true,
                     header_converters: :symbol)
 
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
-  zipcode = row[:zipcode]
 
-  # if the zip code is exactly five digits, assume that it is ok
-  # if the zip code is more than five digits, truncate it to the five digits
-  # if the xip code is lass than five digits, add zeros the the front until it becomes five digits
+  zipcode = clean_zipcode(row[:zipcode])
 
-  if zipcode.nil?
-    zipcode = '00000'
-  elsif zipcode.length < 5
-    zipcode = zipcode.rjust(5, '0')
-  elsif zipcode.length > 5
-    zipcode = zipcode[0..4]
-  end
+  legislators = legislator_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode}"
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id,form_letter)
 end
