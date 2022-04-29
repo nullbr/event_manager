da9b6f5dddce7fe144b254b4e98e3eb692f365de
nullbr <bmarianoleite3@gmail.com>
2022-04-29 10:56:28 -0300

    added CSV library

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -9,7 +9,17 @@ contents.each do |row|
   name = row[:first_name]
   zipcode = row[:zipcode]
 
-  # if the zip code is ex
+  # if the zip code is exactly five digits, assume that it is ok
+  # if the zip code is more than five digits, truncate it to the five digits
+  # if the xip code is lass than five digits, add zeros the the front until it becomes five digits
+
+  if zipcode.nil?
+    zipcode = '00000'
+  elsif zipcode.length < 5
+    zipcode = zipcode.rjust(5, '0')
+  elsif zipcode.length > 5
+    zipcode = zipcode[0..4]
+  end
 
   puts "#{name} #{zipcode}"
 end
