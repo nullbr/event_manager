264872898df71237cd66ee518444c96ba64c3d2a
nullbr <bmarianoleite3@gmail.com>
2022-04-29 21:00:36 -0300

    added peak registration hours and days

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,18 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'time'
+
+def get_reg_time(time)
+  time = "#{time[0..5]}20#{time[6..13]}"
+  Time.strptime(time, '%m/%d/%Y %k:%M')
+end
+
+def get_peak(hash)
+  sorted = hash.sort_by { |_key, value| value }
+  sorted = sorted.last(3)
+  [sorted[0][0], sorted[1][0], sorted[2][0]]
+end
 
 def clean_zipcode(zipcode)
   # if zip code was not filled, it becomes the wrong zip code default of 00000
@@ -11,9 +23,9 @@ def clean_zipcode(zipcode)
 end
 
 def clean_phone_number(n)
-  n = n.to_s.tr("^0-9", '')
+  n = n.to_s.tr('^0-9', '')
 
-  if n.size == 10 
+  if n.size == 10
     # format number to standard phone number format
     "(#{n[0..2]})#{n[3..5]}-#{n[6..10]}"
   elsif n.size == 11 && n[0] == '1'
@@ -36,8 +48,8 @@ def legislator_by_zipcode(zipcode)
     levels: 'country',
     roles: %w[legislatorUpperBody legislatorLowerBody]
   ).officials
-  rescue StandardError
-    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+rescue StandardError
+  'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
 end
 
 def save_thank_you_letter(id, form_letter)
@@ -58,7 +70,10 @@ contents = CSV.open('event_attendees.csv',
 
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
+hours = {}
+weekdays = {}
 
+#=begin
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
@@ -67,9 +82,33 @@ contents.each do |row|
 
   phone_number = clean_phone_number(row[:homephone])
 
-  legislators = legislator_by_zipcode(zipcode)
+  # legislators = legislator_by_zipcode(zipcode)
 
-  form_letter = erb_template.result(binding)
+  # form_letter = erb_template.result(binding)
 
-  save_thank_you_letter(id,form_letter)
+  # save_thank_you_letter(id,form_letter)
+
+  time = get_reg_time(row[:regdate])
+
+  if hours[time.hour].nil?
+    hours[time.hour] = 1
+  else
+    hours[time.hour] += 1
+  end
+
+  if weekdays[time.wday].nil?
+    weekdays[time.wday] = 1
+  else
+    weekdays[time.wday] += 1
+  end
 end
+
+peak_hours = get_peak(hours)
+peak_weekdays = get_peak(weekdays)
+weekdays = %w[Sunday Monday Tuesday Wednesday Thursday Friday Saturday]
+
+days = []
+peak_weekdays.each { |day| days << weekdays[day.to_i] }
+
+puts "Peak registration hours were: #{peak_hours.join(', ')}"
+puts "Peak registration Days were: #{days.join(', ')}"
